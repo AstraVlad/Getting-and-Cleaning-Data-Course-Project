25185b7a4397b94c79922945052859f7b395ae47
Vladimir Feldman <wfel@narod.ru>
2016-07-17 21:37:13 +0300

    Data path is changed

--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,24 @@
 run_analyis <- function(){
 #Loading train dataset
-train <- read.table("./UCI/train/X_train.txt")
+train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 #Making column names comprehensible
-cnames <- read.table("./UCI/features.txt")
+cnames <- read.table("./UCI HAR Dataset/features.txt")
 colnames(train) <- cnames$V2
 #Loading test dataset
-test <- read.table("./UCI/test/X_test.txt")
+test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 #Making column names comprehensible
 colnames(test) <- cnames$V2
 #Loading subjects and activities names for train dataset
-train_subjects <- read.table("./UCI/train/subject_train.txt")
-train_y <- read.table("./UCI/train/y_train.txt")
+train_subjects <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+train_y <- read.table("./UCI HAR Dataset/train/y_train.txt")
 #Making column names comprehensible
 colnames(train_subjects) <- "subjects"
 colnames(train_y) <- "activities"
 #Combining subjects, activities and actual data for training dataset
 train_all <- cbind(train_subjects, train_y, train)
 #Loading subjects and activities names for test dataset
-test_subjects <- read.table("./UCI/test/subject_test.txt")
-test_y <- read.table("./UCI/test/y_test.txt")
+test_subjects <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+test_y <- read.table("./UCI HAR Dataset/test/y_test.txt")
 #Making column names comprehensible
 colnames(test_subjects) <- "subjects"
 colnames(test_y) <- "activities"
@@ -29,7 +29,7 @@ all_data <- rbind(train_all, test_all)
 #Extracting measurements on the mean and standard deviation for each measurement
 mean_std_data <- all_data[ ,grepl("mean\\(\\)|std\\(\\)|activities|subjects", colnames(all_data))]
 #Loading activities
-activities <- read.table("./UCI/activity_labels.txt")
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
 #Making column names comprehensible
 colnames(activities) <- c("activities", "activity_name")
 #Adding descriptive activity names
